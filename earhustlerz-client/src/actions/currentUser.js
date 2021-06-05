c1c4cbc924d6ba7be008d85b3add475203f776c1
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-05 17:53:20 -0400

    added to login fetch request and created/added login form reducer to store

--- earhustlerz-client/src/actions/currentUser.js
+++ earhustlerz-client/src/actions/currentUser.js
@@ -9,6 +9,12 @@ export const setCurrentUser = user => {
 //asynchronous
 export const login = credentials => {
     return dispatch => {
-        return fetch("tcp://localhost:3001")
+        return fetch("tcp://localhost:3001/api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
     }
 }
--- earhustlerz-client/src/store.js
+++ earhustlerz-client/src/store.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import usersReducer from './reducers/users.js'
 import currentUserReducer from './reducers/currentUser.js'
+import loginForm from './reducers/loginForm.js'
 
 const reducer = combineReducers({
     user: usersReducer,
-    currentUser: currentUserReducer
+    currentUserReducer,
+    loginForm
   })
 
   const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
