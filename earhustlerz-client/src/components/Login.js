3e9db2a5cdf7a429244629d6604cd1f3a15e22e3
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-08 00:03:42 -0400

    creating handler for login submission before configuring backend.

--- earhustlerz-client/src/components/Login.js
+++ earhustlerz-client/src/components/Login.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { updateLoginForm } from '../actions/loginForm.js'
+import { login } from '../actions/currentUser.js'
 
-const Login = ({ loginFormData, updateLoginForm }) => {
+const Login = ({ loginFormData, updateLoginForm, login}) => {
 
     const handleInputChange = event => {
         const { name, value } = event.target
@@ -13,8 +14,13 @@ const Login = ({ loginFormData, updateLoginForm }) => {
         updateLoginForm(updatedFormInfo)
       }
 
+      const handleSubmit = event => {
+        event.preventDefault()
+        login(loginFormData)
+      }
+
     return(
-        <form onSubmit={undefined}>
+        <form onSubmit={handleSubmit}>
             <input placeholder="Username" value={loginFormData.username} name="username" type="text" onChange={handleInputChange} />
             <input placeholder="Password" value={loginFormData.password} name="password" type="text" onChange={handleInputChange} />
             <input type="submit" value="Log In" />
@@ -28,4 +34,4 @@ const mapStateToProps = state => {
       }
 }
 
-export default connect(mapStateToProps, {updateLoginForm} )(Login)
+export default connect(mapStateToProps, {updateLoginForm, login} )(Login)
