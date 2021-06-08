e0c71b748aa9c5c3450e0f3cab5e872bfe6ab498
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-07 23:50:09 -0400

    added login component to app.js and built handler for input changes on login form

--- earhustlerz-client/src/App.js
+++ earhustlerz-client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 
 class App extends React.Component {
 
 
   render(){
   return (
-      "Hey Hey it's React"
+      <Login />
   );
   }
 }
--- earhustlerz-client/src/components/Login.js
+++ earhustlerz-client/src/components/Login.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { updateLoginForm } from '../actions/loginForm.js'
 
-const Login = () => {
+const Login = ({ loginFormData, updateLoginForm }) => {
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        const updatedFormInfo = {
+          ...loginFormData,
+          [name]: value
+        }
+        updateLoginForm(updatedFormInfo)
+      }
+
     return(
-        <form onSubmit={}>
-            <input placeholder="Username" value={} name="username" type="text" onChange={} />
-            <input placeholder="Password" value={} name="password" type="text" onChange={} />
+        <form onSubmit={undefined}>
+            <input placeholder="Username" value={loginFormData.username} name="username" type="text" onChange={handleInputChange} />
+            <input placeholder="Password" value={loginFormData.password} name="password" type="text" onChange={handleInputChange} />
             <input type="submit" value="Log In" />
         </form>
     )
 }
 
-export default connect()(Login)
+const mapStateToProps = state => {
+    return {
+        loginFormData: state.loginForm
+      }
+}
+
+export default connect(mapStateToProps, {updateLoginForm} )(Login)
