5e8b80561afedeb9d3c0fa0c8e8f0f858a847ca6
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-08 02:11:11 -0400

    completing fetch for post login and built the fetch to GET the current user when the page loads

--- earhustlerz-client/src/App.js
+++ earhustlerz-client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { getCurrentUser } from "./actions/currentUser.js"
 import Login from './components/Login';
 
 class App extends React.Component {
 
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
 
   render(){
   return (
@@ -12,4 +17,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(null, {getCurrentUser})(App);
