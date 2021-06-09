0cc047306e72604d20005d80c6432a7cc37f8fdb
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-09 14:09:08 -0400

    built get favorites functionality to fetch albums

--- earhustlerz-client/src/actions/favorites.js
+++ earhustlerz-client/src/actions/favorites.js
@@ -1,6 +1,29 @@
+//synchro
 export const setFavorites = favorites => {
     return {
         type: "SET_FAVORITES",
         favorites
+    }
+}
+
+//asynchro
+export const getFavorites = () =>{
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/albums", {
+        credentials: "include",
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+        .then(r => r.json())
+        .then(response => {
+          if (response.error) {
+            console.log(response.error)
+          } else {
+            dispatch(setFavorites(response.data))
+          }
+        })
+        .catch(console.log)
     }
 }
