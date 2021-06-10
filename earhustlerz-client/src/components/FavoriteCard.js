ad151ffeb48dbbe40189562681a8db2ee83c4e62
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-09 20:38:15 -0400

    cleaning up favorite Card display

--- earhustlerz-client/src/components/FavoriteCard.js
+++ earhustlerz-client/src/components/FavoriteCard.js
@@ -3,10 +3,17 @@ import React from 'react'
 const FavoriteCard = ({favorite}) => {
     return (
         favorite ? 
-                <div>
-                  <h3>{favorite.attributes.title}</h3>
-                  <p>{favorite.attributes.artist}</p>
-                  <p>{favorite.attributes.genre}</p>
+                <div class="card">
+                <img class="card-img-top" src={favorite.attributes.cover} alt="Card image cap"></img>
+                <div class="card-body">
+                  <h5 class="card-title">{favorite.attributes.title}</h5>
+                  <h6 class="card-artist">{favorite.attributes.artist}</h6>
+                  <h7 class="card-genre">{favorite.attributes.genre}</h7>
+                </div>
+                <div class="card-body">
+    <a href="#" class="card-link">TrackList</a>
+    <a href="#" class="card-link">See Reviews</a>
+  </div>
                 </div>  : <p>No Card</p>
     )
 }
