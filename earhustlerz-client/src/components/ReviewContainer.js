8c4f49b3d032ed7ec08d7e764388be67d65141fe
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-16 18:01:55 -0400

    using old main container skeleton for reviews container

---
+++ earhustlerz-client/src/components/ReviewContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  from './.js'
+
+const ReviewContainer = () => {
+    return (
+        <div className="ReviewsContainer">
+            <ReviewsList/>
+            <ReviewsForm/>
+        </div>
+    )
+}
+
+export default ReviewContainer
