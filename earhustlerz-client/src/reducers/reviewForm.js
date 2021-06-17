391b5d274fc3c0fce2f30f2ed023426e875aba9e
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-16 20:11:58 -0400

    created review form reducer

---
+++ earhustlerz-client/src/reducers/reviewForm.js
@@ -0,0 +1,18 @@
+const initialState = {
+   content: ""
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case "UPDATE_NEW_REVIEW_FORM":
+            const returnVal =  {
+                ...state,
+                [action.formData.name]: action.formData.value
+            }
+            return returnVal 
+        case "RESET_NEW_REVIEW_FORM":
+            return initialState
+        default:
+            return state
+    }
+}
